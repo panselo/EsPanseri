afcd6b62cab85a41e5edfcd2724d63d36beec4c0
PANSERI TOMMASO <PANSERI.21255>
2024-09-16 12:40:29 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner (System.in);
+        do{
+            System.out.println("1 - aggiunta di una nuova auto");
+            System.out.println("2 - visualizzazione di tutte le auto");
+            System.out.println("3 - ricerca di un'auto tramite marca e modello");
+            System.out.println("4 - cancellazione di un'auto");
+            System.out.println("5 - modifica dei dati di un'auto");
+            System.out.println("6 - visualizzazione delle auto in ordine crescente");
+            System.out.println("7 - visualizzare tutti i modelli che diesel");
+            System.out.println("8 - esci");
+        }while();
+    }
+}
